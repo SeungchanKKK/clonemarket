4e4c52430584f911ec865c53851ff2334804ea09
hoholoudly <hoholoudly@gmail.com>
2022-06-21 16:10:31 +0900

    카테고리 조회 추가

--- src/main/java/com/marketkutty/marketkutty/controller/ProductController.java
+++ src/main/java/com/marketkutty/marketkutty/controller/ProductController.java
@@ -1,15 +1,12 @@
 package com.marketkutty.marketkutty.controller;
 
-import com.marketkutty.marketkutty.model.dto.ProductDto;
+import com.marketkutty.marketkutty.model.dto.responseDto.CategoryRespDto;
 import com.marketkutty.marketkutty.model.dto.responseDto.ProductRespDto;
 import com.marketkutty.marketkutty.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,8 +14,18 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @GetMapping("/api/category")
+    public CategoryRespDto getCategoryList(){
+        return productService.getCategoryList();
+    }
+
     @GetMapping("/api/category/{depth1}")
-    public ProductRespDto getProductList(@PathVariable String depth1){
-        return productService.getProductList(depth1);
+    public ProductRespDto getDepth1List(@PathVariable String depth1){
+        return productService.getDepth1List(depth1);
+    }
+
+    @GetMapping("/api/category/{depth1}/{depth2}")
+    public ProductRespDto getDepth2List(@PathVariable String depth1, @PathVariable String depth2){
+        return productService.getDepth2List(depth1, depth2);
     }
 }
--- src/main/java/com/marketkutty/marketkutty/service/ProductService.java
+++ src/main/java/com/marketkutty/marketkutty/service/ProductService.java
@@ -1,13 +1,11 @@
 package com.marketkutty.marketkutty.service;
 
 
-import com.marketkutty.marketkutty.model.dto.ProductDto;
+import com.marketkutty.marketkutty.model.dto.responseDto.CategoryRespDto;
 import com.marketkutty.marketkutty.model.dto.responseDto.ProductRespDto;
 import com.marketkutty.marketkutty.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,8 +13,16 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public ProductRespDto getProductList(String depth1){
+    public CategoryRespDto getCategoryList(){
+        return productRepository.findCategory();
+    }
+
+    public ProductRespDto getDepth1List(String depth1){
         return productRepository.findAllDepth1(depth1);
+    }
+
+    public ProductRespDto getDepth2List(String depth1, String depth2) {
+        return productRepository.findAllDepth2(depth1, depth2);
     }
 
 }
